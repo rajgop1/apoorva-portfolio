be10a43e48a77b9f7efcc482cde40479cfcca644
aporrva <a9296430@gmail.com>
2025-02-11 06:13:57 +0530

    added interconnection btw pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,1 @@
-const posts = [
-    {
-        title: "Making a design system from scratch",
-        date: "12 Feb 2020",
-        category: "Design, Pattern",
-        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-    },
-    {
-        title: "Creating pixel perfect icons in Figma",
-        date: "12 Feb 2020",
-        category: "Figma, Icon Design",
-        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-    }
-]
-
-console.log(posts)
+let howeron = document.getElementsByClassName("hower")
