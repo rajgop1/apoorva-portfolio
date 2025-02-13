f2f55b669c182975290d7432e34492d90230d791
aporrva <a9296430@gmail.com>
2025-02-13 14:39:35 +0530

    done

--- scripts/apoorva.js
+++ scripts/apoorva.js
@@ -1,24 +1,73 @@
-
-
-
-
+let url = "https://dummyjson.com/products";
 async function getimg () {
-    let url = "https://dummyjson.com/products";
-    let response = await fetch(url);
-        console.log(response)
-    let data = await response.json();
-        console.log(data)
-    let pic = data.products[1].thumbnail; 
-        console.log(pic)
+let num = 0
+
+const maindiv = document.createElement("div")
+maindiv.classList.add("maindiv")
+
+while (num<=29){
 
 
-
-    const image = document.createElement("img");
-    const imgcontainer = document.createElement("div");
+        let imgcontainer = document.createElement("div");
+        imgcontainer.classList.add("imgcontainer");
+    let image = document.createElement("img");
+        image.classList.add("image")
     imgcontainer.append(image);
-    document.body.append(imgcontainer); 
 
     
-    image.src = pic  
+
+
+
+    let about_rating = document.createElement("div");
+        about_rating.classList.add("aboutrate");
+    let ratespan= document.createElement("span");
+        ratespan.classList.add("rate");
+    let rating = document.createElement("span");
+        rating.classList.add("rating");
+    ratespan.textContent = "Rating:  "
+
+    imgcontainer.append(about_rating);
+    about_rating.append(ratespan,rating)
+
+
+
+
+    let sect = document.createElement("section");
+        sect.classList.add("about-sect");
+    let about = document.createElement("span");
+    let warranty = document.createElement("span");
+
+    imgcontainer.append(sect);
+    sect.append(about,warranty);
+
+    maindiv.append(imgcontainer)
+    document.body.append(maindiv);
+
+
+
+    let response = await fetch(url);
+        
+    let data = await response.json();
+       
+    let pic = data.products[num].thumbnail; 
+        
+    image.src = pic
+
+
+    let tag = data.products[num].title;
+   
+    let warran = data.products[num].warrantyInformation;
+  
+    about.textContent= tag
+    warranty.textContent = warran;
+
+    rating.textContent = data.products[num].rating;
+
+ num = num +1
 }
+
+}
+
+
+
 getimg()
